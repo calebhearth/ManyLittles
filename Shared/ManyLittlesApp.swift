5bd7b2fb3d1c2157f5e34d1ed0118e395fa7232a
Caleb Hearth <caleb@calebhearth.com>
2021-12-18 17:16:18 -0700

    Initial Commit

---
+++ Shared/ManyLittlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyLittlesApp.swift
+//  Shared
+//
+//  Created by Caleb Hearth on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ManyLittlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
